f86c19284b25db045ad88c706d99d378c14ee67d
maureenmlee <maureen.ml.lee@gmail.com>
2021-04-25 23:27:04 -0700

    Pass all tests

--- src/main/java/Playlist.java
+++ src/main/java/Playlist.java
@@ -1,4 +1,15 @@
 import java.util.ArrayList;
 
 public class Playlist {
+    public String name;
+    ArrayList<String> list = new ArrayList<String>();
+
+    public Playlist(String name) {
+        this.name = name;
+    }
+
+//    public Playlist() {
+//        this.name = "Null";
+//    }
+
 }
